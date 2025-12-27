6224c99ec65118291e9cefb091747e9b7f778911
AthulMuraliT <Athul.MuraliT@ust.trumio.com>
2025-12-27 09:46:03 +0530

    start

---
+++ ai-tools-backend/src/test/java/com/aitoolfinder/ai_tools_backend/AiToolFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aitoolfinder.ai_tools_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiToolFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
